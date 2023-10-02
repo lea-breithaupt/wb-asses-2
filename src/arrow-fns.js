000a1be722c5f2aaf1953523874fa70ce9aabe3c
lea-breithaupt <l.breithaupt3@gmail.com>
2023-10-02 08:57:55 -0600

    second commit

---
+++ src/arrow-fns.js
@@ -0,0 +1,72 @@
+// Unit 2 Assessment: arrow-fns.js
+
+// Set the value of giveMeTwo to an arrow function that returns 2.
+//
+// Ex.:
+//   giveMeTwo();
+//   => 2
+const giveMeTwo = (num) => num = 2
+
+// Set the value of addNums to an arrow function that takes in two numbers and
+// returns their sum.
+//
+// Ex.:
+//   addNums(1, 2);
+//   => 3
+const addNums = (num1, num2) => num1 + num2
+
+// Set the value of max to an arrow function that takes in two numbers and
+// returns the largest one.
+//
+// Ex.:
+//   max(1, 2);
+//   => 2
+const max = (num1, num2) => Math.max(num1, num2)
+
+// Note: you can also do this on one line:
+// const max = (a, b) => (a >= b ? a : b);
+
+// Given an array of numbers, return a new array that only includes the even
+// numbers. Do this using array iteration methods. Do NOT use a for loop.
+//
+// Ex.:
+//   evens([1, 2, 3, 4, 5]);
+//   => [2, 4]
+function evens(nums) {
+    let evenNums = []
+    nums.forEach(num => {
+        if(num %2===0)
+        evenNums.push(num)
+    }); return evenNums
+}
+
+// Given an array of names, return a new array of greetings for each name. Do
+// this using array iteration methods. Do NOT use a for loop.
+//
+// Ex.:
+//   createGreetings(['Clive', 'Jill', 'Torgal']);
+//   => ['Hello, Clive!', 'Hello, Jill!', 'Hello, Torgal!']
+function createGreetings(names) {
+    let greeting = []
+    names.map(value => {
+        'Hello,' + value + '!'
+    }); return greeting
+}
+
+// Given an array of words, return a new array where each word is uppercased
+// and only includes words that are longer than 4 characters. Do this using
+// array iteration methods (you may need more than one). Do NOT use a for loop.
+//
+// Ex.:
+//   loudLongWords(['apple', 'pear', 'cake', 'pinata']);
+//   => ['APPLE', 'PINATA']
+function loudLongWords(words) {
+    let capitalizedWords = []
+        words.forEach(word => {
+        if(word.length > 4){
+            capitalizedWords.push(word.toUpperCase())
+        } 
+    }); return capitalizedWords
+}
+
+export { addNums, createGreetings, evens, giveMeTwo, loudLongWords, max };
